a666432120f1554509e682c4b3aceefc288062fb
jaisonrobson <xd_jrg@hotmail.com>
2021-08-06 00:23:27 -0300

    #35 - Componentes com estado

--- src/components/comunication/IndiretaFilho.jsx
+++ src/components/comunication/IndiretaFilho.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
 export default props => {
+    const min = 50
+    const max = 70
+    const gerarIdade = () => parseInt(Math.random() * (max - min)) + min
+
     return (
         <div>
             <div>
                 Filho
             </div>
-            <button onClick={props.onClick('Joao', 51, true)}>Fornecer Informacoes</button>
+            <button onClick={() => props.onClick('Joao', gerarIdade(), true)}>Fornecer Informacoes</button>
         </div>
     )
 }
--- src/components/comunication/IndiretaPai.jsx
+++ src/components/comunication/IndiretaPai.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import IndiretaFilho from './IndiretaFilho'
 
 export default props => {
+    const [nome, setNome] = useState('?')
+    const [idade, setIdade] = useState(0)
+    const [bool, setBool] = useState(false)
 
-    function fornecerInformacoes(nome, idade, nerd) {
-
+    function fornecerInformacoes(nomeParam, idadeParam, boolParam) {
+        setNome(nomeParam)
+        setIdade(idadeParam)
+        setBool(boolParam)
     }
 
     return (
         <div>
-            Pai
+            <div>
+                <span>{nome + ' '}</span>
+                <span><strong>{idade + ' '}</strong></span>
+                <span>{bool ? 'Verdadeiro' : 'Falso'}</span>
+            </div>
             <IndiretaFilho onClick={fornecerInformacoes} />
         </div>
     )
